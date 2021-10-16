a730eddc01ba811ee45407d950043d8ebaf92198
Santiago Giraldo <santiagogil271@gmail.com>
2021-10-16 09:18:28 -0500

    agregando relaciones eloquent y modelos

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function Rol(){
+        return $this->belongsTo('App\Models\Rol', 'id_rol');
+    }
 }
