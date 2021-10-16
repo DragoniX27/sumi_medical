e56f6f1b8bf1bbd627dfdc2bf29d99c4600c1cf6
Santiago Giraldo <santiagogil271@gmail.com>
2021-10-16 08:12:29 -0500

    agregando migracion de base de datos y instalacíion de Vue y Vuetify

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +13,59 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('roles')->insert([
+            'nombre' => 'administrador'
+        ]);
+
+        DB::table('roles')->insert([
+            'nombre' => 'usuario'
+        ]);
+
+        DB::table('permisos')->insert([
+            'permiso' => 'Crear'
+        ]);
+
+        DB::table('permisos')->insert([
+            'permiso' => 'Actualizar'
+        ]);
+
+        DB::table('permisos')->insert([
+            'permiso' => 'Leer'
+        ]);
+
+        DB::table('permisos')->insert([
+            'permiso' => 'Eliminar'
+        ]);
+
+        for($i = 1; $i <= 4; $i++){
+            DB::table('roles_has_permisos')->insert([
+                'id_rol' => 1,
+                'id_permiso'=> $i
+            ]);
+        }
+
+        DB::table('roles_has_permisos')->insert([
+            'id_rol' => 2,
+            'id_permiso'=> 1
+        ]);
+
+        DB::table('roles_has_permisos')->insert([
+            'id_rol' => 2,
+            'id_permiso'=> 3
+        ]);
+
+        DB::table('users')->insert([
+            'id_rol' => 1,
+            'name' => 'administrador',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin123'),
+        ]);
+
+        DB::table('users')->insert([
+            'id_rol' => 2,
+            'name' => 'Juan Carlos Bodoque',
+            'email' => 'bodoque@31minutos.es',
+            'password' => bcrypt('123456'),
+        ]);
     }
 }
